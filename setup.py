b56daa981175b78cf8a190a51355ec6a99467d5c
ant30 <ant30tx@gmail.com>
2012-08-01 13:49:58 +0200

    fix another typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Copyright 2012 Rooter. All rights reserved.
 import fnmatch
 from setuptools import setup, find_packages
-import sys, os
+import os
 
 
 
@@ -24,7 +24,7 @@ version = '0.0'
 setup(name='askbot-openmooc/',
       version=version,
       description=("Askbot openmooc integration (theme, saml2, tags api)"),
-      long_description=(read('README.rst') + '\n\n' + read('CHANGES.rst')),,
+      long_description=(read('README.rst') + '\n\n' + read('CHANGES.rst')),
       classifiers=[
         'Development Status :: 6 - Development',
         'Environment :: Web Environment',
