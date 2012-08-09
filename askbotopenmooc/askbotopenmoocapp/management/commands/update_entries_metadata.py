922089b8a916b3f6fd8021d0c001252955492a91
ant30 <ant30tx@gmail.com>
2012-08-09 08:15:13 +0200

    Update entries metadata generate a valid xml

---
+++ askbotopenmooc/askbotopenmoocapp/management/commands/update_entries_metadata.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from django.conf import settings
+from os import listdir
+from os import path
+from urllib2 import urlopen
+from urlparse import urljoin
+
+
+HEADER="""<?xml version='1.0' encoding='UTF-8'?>
+<md:EntitiesDescriptor
+    xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
+    xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
+    xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
+    entityID="https://idp.example.org/SAML2">
+"""
+
+FOOTER="""
+</md:EntitiesDescriptor>
+"""
+
+class Command(BaseCommand):
+    args = 'courses_basedir'
+    help = 'Set a user as moderator'
+    requires_model_validation = False
+
+    def handle(self, *args, **options):
+        if not len(args):
+            raise CommandError('You must specific courses base dir\n')
+
+        course_basedir = args[0]
+        metadatafile = open(path.join(settings.STATIC_ROOT, 'group-metadata.xml'), 'w')
+        metadatafile.write(HEADER)
+        for course in listdir(course_basedir):
+            if course == 'skel':
+                continue
+            url = urljoin(settings.FULL_ASKBOT_URL, '%s/saml2/metadata/' % course)
+            self.stdout.write("Fetching %s\n" % url)
+            metadata_request = urlopen(url)
+            metadatacontent = metadata_request.read()
+            metadatacontent = metadatacontent.replace(
+                        "<?xml version='1.0' encoding='UTF-8'?>", "")
+            metadatafile.write(metadatacontent)
+        metadatafile.write(FOOTER)
+        metadatafile.close()
+
+
+
+
