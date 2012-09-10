213e5215b717345105fb132e7d3c41407e9a7e20
ant30 <ant30tx@gmail.com>
2012-09-10 10:45:49 +0200

    Fix redirect loop on saml forced login

--- askbotopenmooc/askbotopenmoocapp/middlewares.py
+++ askbotopenmooc/askbotopenmoocapp/middlewares.py
@@ -1,3 +1,5 @@
+from urlparse import urlparse
+
 from django.conf import settings
 from django.contrib.auth.views import redirect_to_login
 
@@ -10,6 +12,13 @@ class Saml2SSORedirect(object):
     def process_request(self, request):
         if request.user.is_authenticated():
             return None
+
+        if 'HTTP_REFERER' in request.META:
+            url_referer = urlparse(request.META['HTTP_REFERER'])
+            for idp in settings.SAML_CONFIG['service']['sp']['idp'].keys():
+                idp_url = urlparse(idp)
+                if idp_url.hostname == url_referer.hostname:
+                    return None
 
         if (hasattr(settings, 'SAML2_COOKIE') and request.COOKIES and
             settings.SAML2_COOKIE in request.COOKIES and
