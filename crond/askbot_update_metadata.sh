6d72c8710806805b47f50e01c3bd23fd846340ff
ant30 <ant30tx@gmail.com>
2012-09-10 16:43:25 +0200

    Enable virtualenv to download metadatas

--- crond/askbot_update_metadata.sh
+++ crond/askbot_update_metadata.sh
@@ -9,6 +9,9 @@ PATH_TO_ASKBOT_OPENMOOC='/home/mooc/askbot-openmooc'
 
 cd $PATH_TO_ASKBOT_OPENMOOC
 
-mkdir -p /tmp/askbot-openmooc/
-python manage.py update_entries_metadata &> /tmp/askbot-openmooc/update_entries_metadata.log
+su - mooc -c "mkdir -p /tmp/askbot-openmooc/"
+su - mooc -c "cd $PATH_TO_ASKBOT_OPENMOOC && \
+              source $PYTHON_VIRTUAL_ENV/bin/activate && \
+              python manage.py update_entries_metadata \
+                 &> /tmp/askbot-openmooc/update_entries_metadata.log"
 
