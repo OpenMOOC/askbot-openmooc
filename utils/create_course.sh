614a9699ecd9056f4d72e265a0a79b5d66641205
ant30 <ant30tx@gmail.com>
2012-10-15 17:05:41 +0200

    Load course fixtures if their exists. Can be livesettings.json, auth.json...

--- utils/create_course.sh
+++ utils/create_course.sh
@@ -34,7 +34,12 @@ mkdir -p courses/$COURSE/upfiles
 cd courses/$COURSE
 
 sed "s/^DATABASE_NAME.*$/DATABASE_NAME = '$DB_NAME'/g" -i course_settings.py
-python manage.py syncdb --migrate
+if [ -d fixtures ]; then
+        python manage.py syncdb --migrate --noinput
+        find fixtures -type f -exec python manage.py loaddata {} \;
+else
+        python manage.py syncdb --migrate --noinput
+fi
 
 echo "Give user/teacher (email) moderator"
 read -p Email: EMAIL
