1907a3a92b22bd93c6b2f4c62ff3e5da2c3a3704
ant30 <ant30tx@gmail.com>
2012-10-29 12:12:27 +0100

    Temporary auth backend, to allow fix users

--- askbotopenmooc/askbotopenmoocapp/backends.py
+++ askbotopenmooc/askbotopenmoocapp/backends.py
@@ -6,17 +6,19 @@ logger = logging.getLogger('djangosaml2')
 
 class CustomSaml2Backend(Saml2Backend):
 
-    def update_user2(self, user, attributes, attribute_mapping,
-                    force_save=False):
-        super(CustomSaml2Backend, self).update_user(user, attributes,
-                                                    attribute_mapping,
-                                                    force_save)
-
-        realname = " ".join([user.first_name, user.last_name])
-        if user.realname != realname:
-            user.realname = realname
-            if force_save:
-                user.save()
+    #def update_user(self, user, attributes, attribute_mapping,
+    #                force_save=False):
+    #    super(CustomSaml2Backend, self).update_user(user, attributes,
+    #                                                attribute_mapping,
+    #                                                force_save)
+    #    logger.debug(unicode(user))
+    #    realname = "%s %s" % (user.first_name, user.last_name)
+    #    realname = realname[:70]
+    #    if user.username != realname:
+    #        user.username = realname
+    #        if force_save:
+    #            user.save()
+    #    logger.debug(unicode(user))
 
     def authenticate(self, session_info=None, attribute_mapping=None,
                      create_unknown_user=True):
@@ -32,7 +34,7 @@ class CustomSaml2Backend(Saml2Backend):
 
     def update_user(self, user, attributes, attribute_mapping,
                     force_save=False):
-        if user.username != user.email:
+        if user and user.username and user.username != user.email:
             return user
         else:
             return super(CustomSaml2Backend, self).update_user(user, attributes,
