d48b0d0897cd74d0f3e07b7ad4a9fe6796837f9b
ant30 <ant30tx@gmail.com>
2012-10-31 17:23:48 +0100

    Refactoring username generator

--- askbotopenmooc/askbotopenmoocapp/__init__.py
+++ askbotopenmooc/askbotopenmoocapp/__init__.py
@@ -1,19 +1,33 @@
+from django.contrib.auth.models import User
+
 from djangosaml2.signals import pre_user_save
+
 from askbotopenmooc.askbotopenmoocapp.utils import generate_unique_username
+
+
+def change_username(user, candidate):
+    if User.objects.filter(username=candidate).counts() > 0:
+        candidate = generate_unique_username(candidate)
+    user.username = candidate
 
 
 def askbot_pre_user_save(sender, **kwargs):
     user = sender
-    realname = u"%s %s" % (user.first_name.strip(), user.last_name.strip())
-    realname = realname.strip()
-    realname = realname[:75]
-    if realname and user.username != realname:
-        if user.username and user.username.startswith(realname):
-            # User already has a unique username
-            return False
-        realname = generate_unique_username(realname, 75)
-        if user.username != realname:
-            user.username = realname
+    candidate = u"%s %s" % (user.first_name.strip(), user.last_name.strip())
+    candidate = candidate.strip()
+    candidate = candidate[:75]
+
+    # if user is a new user
+    if user.username is None:
+        change_username(user, candidate)
+        return True
+    else:
+        user_old = User.objects.get(id=user.id)
+        # If user has changed his first_name or last_name
+        if ((user_old.first_name != user.first_name) or
+            (user_old.last_name != user.last_name)):
+
+            change_username(user, candidate)
             return True
 
 
