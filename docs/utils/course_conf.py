bfac4677f7fac21ed833d009b36a8d4b2233c848
ant30 <ant30tx@gmail.com>
2013-02-13 12:51:03 +0100

    Include nginx config per files

---
+++ docs/utils/course_conf.py
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+from optparse import OptionParser
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+                      help="FILE to write conf")
+    parser.add_option("-t", "--template",
+                      action="store", dest="template",
+                      help="template file")
+    
+    parser.add_option("-p", "--port",
+                      action="store", dest="port",
+                      help="application port")
+    
+    parser.add_option("-n", "--name",
+                      action="store", dest="name",
+                      help="application name")
+    
+    (options, args) = parser.parse_args()
+    
+    if not(options.filename and options.template and options.port and options.name):
+       parser.print_usage()
+
+    with open(options.template, "r") as templatefile:
+        template = templatefile.read()
+
+
+    # .format does not run because templates are very complex
+    render = template.replace("{name}", options.name)
+    render = render.replace("{port}", options.port)
+
+
+    with open(options.filename, "w") as conffile:
+        conffile.write(render)
