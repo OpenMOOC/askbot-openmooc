f9b1c09fca453e577d0bb093328a2422784a5e4f
Oscar Carballal Prego <ocarballal@yaco.es>
2013-08-05 08:35:39 +0200

    Minor first level changes

--- askbotopenmooc/askbotopenmoocapp/middlewares.py
+++ askbotopenmooc/askbotopenmoocapp/middlewares.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012-2013 Rooter Analysis S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from urlparse import urlparse
 
 from django.conf import settings
@@ -13,8 +28,9 @@ from askbot.middleware.forum_mode import is_view_protected, is_view_allowed
 
 
 class Saml2SSORedirect(object):
-    """ Redirect to idp if saml2 cookie is present
-        to make sure that user is logged
+    """
+    Redirect to idp if saml2 cookie is present
+    to make sure that user is logged
     """
 
     def process_request(self, request):
--- askbotopenmooc/urls.py
+++ askbotopenmooc/urls.py
@@ -1,6 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012-2013 Rooter Analysis S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
-main url configuration file for the askbot site
+Main URL configuration file for the askbot site
 """
+
 from django.conf.urls.defaults import patterns, include, handler404, handler500, url
 from django.conf import settings
 
@@ -8,20 +24,28 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
     (r'%s' % settings.ASKBOT_URL, include('askbot.urls')),
+
     (r'^admin/', include(admin.site.urls)),
+
     #(r'^cache/', include('keyedcache.urls')), - broken views disable for now
+
     (r'^settings/', include('askbot.deps.livesettings.urls')),
+
     (r'^followit/', include('followit.urls')),
+
     (r'^robots.txt$', include('robots.urls')),
+
     url( # TODO: replace with django.conf.urls.static ?
-        r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 
+        r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],
         'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT.replace('\\','/')},
     ),
 
     #saml2
     (r'^saml2/', include('djangosaml2.urls')),
+
     (r'^samltest/', 'djangosaml2.views.echo_attributes'),
 
     #Mooc App
